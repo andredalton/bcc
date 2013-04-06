ac10629c86325c296ac4626acb4377030c9dbe0b
Andre Meneghelli Vale <avale@kakaroto>
2013-04-06 19:56:37 -0300

    Concorrente 2013- EP1: Quase tudo ok. Falha de segmentação provavaelmente por sessão crítica.

--- 2013/mac0438/ep1/ep1.c
+++ 2013/mac0438/ep1/ep1.c
@@ -14,114 +14,129 @@ int PortalT2Ent;
 int PortalT1Sai;
 int PortalT2Sai;
 int *estrada[3][180];
-PosicaoAtleta **tempoEspaco;
+PosicaoAtleta **tempoEspaco = NULL;
 
-int tic = 0;					/* O tic de tempo para impressão da classificação. ( default 30min, com -debug 1min ). */
-int lPrint = 0;
+int tic;		/* O tic de tempo para impressão da classificação. ( default 30min, com -debug 1min ). */
+int lPrint;
 int run;
+int natletas;
+int deltaTime;
 
 /* Termino da declaração das variáveis globais. */
 
-/*
-int carregaArquivo(char nome[], int &hp, &mp, &ha, &ma, ){
-	FILE *ent = fopen(nome, "r");
-	
-	
+void imprimeClassificacao(){
+	printf("Imprimindo linha: %d\n", tic);
 }
-*/
 
 /* Imprime a possição dos atletas a cada t segundos de prova. */
 void *classificacao(int n) {
-	int i=5;
+	int i;
 	
-
 	while(run){
-		for(i=0; i<n; i++)
-			if(tempoEspaco[tic][i].id==0) break;
-		
-		printf("(%d %d) \n", tic, i);
+		if( (tic-lPrint)>0 ){
+			for(i=0; i<n; i++)
+				if(tempoEspaco[tic][i].id==-1) break;
+			/* Inicio da sessao critica. */
+			if(i==n){
+				imprimeClassificacao();
+				lPrint++;
 
-		if(i==n){
-			printf("Imprimindo linha: %d\n", tic);
-			tic++;
+				tempoEspaco = reallocX(tempoEspaco[1], sizeof(PosicaoAtleta*)*(tic-lPrint));
+			}
+			/* Termino da sessao critica. */
 		}
 		sleep(1);
 	}
 
-	printf("Ahhhhhh\n");
+	printf("Classificacao final:\n");
 	
-	/*
-	relatividade = reallocX(NULL, sizeof(PosicaoAtleta)*m);
-	relatividade = reallocX(relatividade, sizeof(PosicaoAtleta)*m++);
-	relatividade = reallocX(relatividade, sizeof(PosicaoAtleta)*m++);
-	relatividade = reallocX(relatividade, sizeof(PosicaoAtleta)*m++);
-	relatividade = reallocX(relatividade, sizeof(PosicaoAtleta)*m++);
-	relatividade = reallocX(relatividade[1], sizeof(PosicaoAtleta)*m--);
-	relatividade = reallocX(relatividade[1], sizeof(PosicaoAtleta)*m--);
-	*/
-
 	return NULL;
 }
 
 void *atleta(Atleta a){
 	int i;
-	double p = 0;
+	int t;
+	double p;
+	/* Natação */
+	printf("B\n");
+	for(i=0; i<NATAM; i++){
+		printf("C\n");
+		natacao(a);
+		printf("D\n");
+		if( a->ms[NATACAO]/deltaTime != t/deltaTime ){
+			/* Inicio da sessao critica. */
+			if( a->ms[NATACAO]/deltaTime > tic ){
+				tic++;
+				printf("F\n");
+				tempoEspaco = reallocX(tempoEspaco, sizeof(PosicaoAtleta*)*(tic+1-lPrint) );
+				tempoEspaco[tic-lPrint] = novasPossicoes(natletas);
+			}
+			/* Aqui acaba a sessao critica. */
 
-	for(i=0; i<10; i++){
-		/*
-		printf("AA\t%d %f\n", a->id, p);
-		atualizaPosicao( &tempoEspaco[i][a->id], a->id, p );
-		*/
-		tempoEspaco[i][a->id].id = a->id;
-		tempoEspaco[i][a->id].posicao = p;
-		printf("\t\t[%d, %d] = %f\n", i, a->id, p);
-		p += (double) (rand()%500);
-		sleep(rand()%5);
+			printf("E\n");
+			p = 100*(i-1) + 100*( (a->ms[NATACAO]-a->ms[NATACAO]%deltaTime)-t)/(a->ms[NATACAO]-t);
+			atualizaPosicao( &tempoEspaco[i][a->id], a->id, p );
+		}
 	}
+
+	/* T1 */
+
+	/* Ciclismo */
+
+	/* T2 */
+
+	/* Corrida */
 
 	return NULL;
 }
 
 /* Função principal de verdade */
 int ironMain(int argc, char *argv[]){
+	char *filename;
 	int
 		i,
 		j,
 		n=10;
 	
-	pthread_t ids_atletas[10];
+	pthread_t *ids_atletas;
 	pthread_t id_classificacao;
 	
 	srand( time(NULL) );
 
-	tempoEspaco = (PosicaoAtleta**) mallocX(10*sizeof(PosicaoAtleta*));
-	
-	printf("A\n");
+	/* Inicializando variáveis globais. */
+	tempoEspaco = NULL;	
+	tic = 0;
+	lPrint = 0;
+	natletas = 10;
+	deltaTime = 1800000;
+	run = 1;
 
-	for(i=0; i<10; i++){
-		printf("B\n");
-		tempoEspaco[i] = novasPossicoes(n);
-		for (j = 0; j < n; ++j)
-		{
-			tempoEspaco[i][j].id = 0;
-			tempoEspaco[i][j].posicao = 0;
-		}
+	for( i=1; i<argc; i++){
+		if( strcmp(argv[i], "-debug")==0 )
+			deltaTime = 60000;
+		else
+			filename = argv[i];
 	}
 
-	run = 1;
+	/*	
+	if(filename==NULL)
+		return 1;
+	*/
 
 	printf("Inicio das threads.\n");
 
 	pthread_create(&(id_classificacao),NULL,  classificacao, (void *)n);
 
-	for (i = 0; i < 10; i++) {
+	ids_atletas = mallocX(natletas*sizeof(pthread_t));
+	for (i=0; i<natletas; i++) {
+		printf("A\n");
 		if (pthread_create(&(ids_atletas[i]),NULL, atleta, (void *) novoAtleta(FEMININO, PROFISSIONAL, i) )) {
 			fprintf(stderr,"Erro no pthread_create\n");
 			return(2);
 		}
 	}
 	
-	for(i=0;i < 10;i++)
+	for(i=0; i<natletas; i++)
 		pthread_join(ids_atletas[i],NULL); /* Esperara a junção das threads */
 
 	run = 0;
@@ -129,18 +144,7 @@ int ironMain(int argc, char *argv[]){
 	pthread_join(id_classificacao,NULL);
 
 	printf("Termino de todas as threads.\n");
-	/*
-	for( i=1; i<argc; i++){
-		if( strcmp(argv[i], "-debug")==0 )
-			deltaTime = 60000;
-		else
-			filename = argv[i];
-	}
 	
-	if(filename==NULL)
-		return 1;
-	*/
-
 	return 0;
 }
 
