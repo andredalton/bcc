304b0db7d1897ff07493bc62036e2eda90af2a5c
Andre Meneghelli Vale <avale@kakaroto>
2013-04-12 21:39:29 -0300

    2013 - Concorrente : EP1 Impressao por grupos quase correta. algum BUG com o tempo final.

--- 2013/mac0438/ep1/ep1.c
+++ 2013/mac0438/ep1/ep1.c
@@ -43,46 +43,73 @@ Atleta *atletas;
 
 /* Imprime a classificação dos atletas.
  **************************************/
-void imprimeClassificacao(int lPrint, int tempo){
-	int i, j;
+void imprimeClassificacao(PosicaoAtleta *lista, int tempo){
+	int i, j, tam;
 	Tempo t;
+	char sex, cat;
 	
 	for( i=0; i<natletas; i++){
 		if(!debug && i>=3) break;
+
+		sex = atletas[lista[i].id]->sexo==MASCULINO ? 'M' : 'F';
+		cat = atletas[lista[i].id]->categoria==PROFISSIONAL ? 'P' : 'A';
+
 		if(tempo) {
-			t = converteTempo(tempoEspaco[lPrint][i].ms);
+			t = converteTempo(lista[i].ms);
 			/*
-			printf(" %d) \"%s %s\"\t%dh%dm%06.3fs\n%d\n", (i+1), atletas[tempoEspaco[lPrint][i].id]->nome, atletas[tempoEspaco[lPrint][i].id]->sobrenome, t->h, t->m, (double)t->ms/1000, strlen(atletas[tempoEspaco[lPrint][i].id]->sobrenome) );
+			printf(" %d) \"%s %s\"\t%dh%dm%06.3fs\n%d\n", (i+1), atletas[lista[i].id]->nome, atletas[lista[i].id]->sobrenome, t->h, t->m, (double)t->ms/1000, strlen(atletas[lista[i].id]->sobrenome) );
 			*/
-			printf("%4d) \"%s %s\"", (i+1), atletas[tempoEspaco[lPrint][i].id]->nome, atletas[tempoEspaco[lPrint][i].id]->sobrenome);
-			for (j=0; j<22-strlen(atletas[tempoEspaco[lPrint][i].id]->nome)-strlen(atletas[tempoEspaco[lPrint][i].id]->sobrenome); j++) {
+			printf("%4d) \"%s %s\"", (i+1), atletas[lista[i].id]->nome, atletas[lista[i].id]->sobrenome);
+			tam = strlen(atletas[lista[i].id]->nome)+strlen(atletas[lista[i].id]->sobrenome);
+			for (j=0; j<22-tam; j++) {
 				printf(" ");
 			}
-			printf("%02dh%02dm%06.3fs\n", t->h, t->m, (double)t->ms/1000);
+			printf("%c   %c   %02dh%02dm%06.3fs\n", sex, cat, t->h, t->m, (double)t->ms/1000);
 			free(t);
 		}
 		else {
 			/*
-			printf(" %d) \"%s %s\"\t%.2fm\n", (i+1), atletas[tempoEspaco[lPrint][i].id]->nome, atletas[tempoEspaco[lPrint][i].id]->sobrenome, tempoEspaco[lPrint][i].posicao);
+			printf(" %d) \"%s %s\"\t%.2fm\n", (i+1), atletas[lista[i].id]->nome, atletas[lista[i].id]->sobrenome, lista[i].posicao);
 			*/
-			printf("%4d) \"%s %s\"", (i+1), atletas[tempoEspaco[lPrint][i].id]->nome, atletas[tempoEspaco[lPrint][i].id]->sobrenome);
-			for (j=0; j<30-log10(tempoEspaco[lPrint][i].posicao)-strlen(atletas[tempoEspaco[lPrint][i].id]->nome)-strlen(atletas[tempoEspaco[lPrint][i].id]->sobrenome); j++) {
+			printf("%4d) \"%s %s\"", (i+1), atletas[lista[i].id]->nome, atletas[lista[i].id]->sobrenome);
+			tam = log10(lista[i].posicao) + strlen(atletas[lista[i].id]->nome)+strlen(atletas[lista[i].id]->sobrenome);
+			for (j=0; j<30-tam; j++) {
 				printf(" ");
 			}
-			printf("%.2fm\n", tempoEspaco[lPrint][i].posicao);
+			printf("%c   %c   ", sex, cat);
+			printf("%.2fm\n", lista[i].posicao);
 		}
 	}
 }
 
 /* Função usada na thread que imprime a classificação dos atletas a cada tic de 
  * tempo.
+ *
+ * Obs: esta função imprime uma nova linha somente quando todos os atletas
+ * finalizam o tic. Não para cada grupo.
  ******************************************************************************/
 void *classificacao(void) {
-	int i;
-	int lPrint=0;			/* Numero do tic impresso (1 ou 30min). */
-	int vencedores = 0;
+	int
+		i,
+		mp,
+		fp,
+		ma,
+		fa,
+		lPrint=0,			/* Numero do tic impresso (1 ou 30min). */
+		vmp = 0,
+		vfp = 0,
+		vma = 0,
+		vfa = 0;
 
-	while( lPrint<(29*debug+1)*TMAX && !vencedores ){
+	PosicaoAtleta **grupos;
+
+	if(!debug){
+		grupos = (PosicaoAtleta **) mallocX(4*sizeof(PosicaoAtleta *));
+		for(i=0; i<4; i++)
+			grupos[i] = novasPossicoes(natletas);
+	}
+
+	while( lPrint<(29*debug+1)*TMAX && ! (vmp && vfp && vma && vfa) ){
 		printf("\r");
 		for(i=0; i<natletas; i++)
 			if( tempoEspaco[lPrint][i].id==-1) break;
@@ -91,33 +118,107 @@ void *classificacao(void) {
 			if(debug) {
 				for(i=0; i<natletas; i++)
 					if( tempoEspaco[lPrint][i].posicao != 100*NATAM + 1000*CITAM + 1000*COTAM) break;
-				if( i==natletas ) vencedores=1;
+				if( i==natletas ) vmp = vfp = vma = vfa = 1;
 				printf("\nClassificacao %dmin:\n", lPrint+1);
+				imprimeClassificacao(tempoEspaco[lPrint], 0);
 			}
 			else {
-				for(i=0; i<3; i++)
-					if( tempoEspaco[lPrint][i].posicao != 100*NATAM + 1000*CITAM + 1000*COTAM) break;
-				if(i==3 || i==natletas)
-					vencedores=1;
-				printf("\nClassificacao %dmin:\n", 30*(lPrint+1));
+				/* Fragmentando os resultados já ordenados em grupos. */
+				mp = fp = ma = fa = 0;
+				for(i=0; i<natletas; i++){
+					switch( atletas[tempoEspaco[lPrint][i].id]->sexo + 2*atletas[tempoEspaco[lPrint][i].id]->categoria ){
+						case MASCULINO + 2 * PROFISSIONAL:
+							if(!vmp) {
+								atualizaPosicao( &grupos[MASCULINO + 2 * PROFISSIONAL][mp], tempoEspaco[lPrint][i].id, tempoEspaco[lPrint][i].ms, tempoEspaco[lPrint][i].posicao );
+								mp++;
+							}
+							break;
+						case FEMININO + 2 * PROFISSIONAL:
+							if(!vfp) {
+								atualizaPosicao( &grupos[FEMININO + 2 * PROFISSIONAL][fp], tempoEspaco[lPrint][i].id, tempoEspaco[lPrint][i].ms, tempoEspaco[lPrint][i].posicao );
+								fp++;
+							}
+							break;
+						case MASCULINO + 2 * AMADOR:
+							if(!vma) {
+								atualizaPosicao( &grupos[MASCULINO + 2 * AMADOR][ma], tempoEspaco[lPrint][i].id, tempoEspaco[lPrint][i].ms, tempoEspaco[lPrint][i].posicao );
+								ma++;
+							}
+							break;
+						case FEMININO + 2 * AMADOR:
+							if(!vfa) {
+								atualizaPosicao( &grupos[FEMININO + 2 * AMADOR][fa], tempoEspaco[lPrint][i].id, tempoEspaco[lPrint][i].ms, tempoEspaco[lPrint][i].posicao );
+								fa++;
+							}
+							break;
+					}
+				}
+
+				/* Verificando o término de cada um dos grupos em separado. */
+				for(i=0; i<3 && !vmp; i++)
+					if( grupos[MASCULINO + 2 * PROFISSIONAL][i].posicao != 100*NATAM + 1000*CITAM + 1000*COTAM) break;
+				if(i==3 || i==mp)
+					vmp=1;
+
+				for(i=0; i<3 && !vfp; i++)
+					if( grupos[FEMININO + 2 * PROFISSIONAL][i].posicao != 100*NATAM + 1000*CITAM + 1000*COTAM) break;
+				if(i==3 || i==mp)
+					vfp=1;
+
+				for(i=0; i<3 && !vma; i++)
+					if( grupos[MASCULINO + 2 * AMADOR][i].posicao != 100*NATAM + 1000*CITAM + 1000*COTAM) break;
+				if(i==3 || i==mp)
+					vma=1;
+
+				for(i=0; i<3 && !vfa; i++)
+					if( grupos[FEMININO + 2 * AMADOR][i].posicao != 100*NATAM + 1000*CITAM + 1000*COTAM) break;
+				if(i==3 || i==mp)
+					vfa=1;
+
+				/* Imprimindo a classificação ou finalização quando necessário pra cada um dos grupos. */
+				if(!vmp){
+					printf("\nClassificacao %dmin:  (Masculino Profissional)\n", 30*(lPrint+1));
+					imprimeClassificacao(grupos[MASCULINO + 2 * PROFISSIONAL], 0);
+				}
+				else if(vmp==1){
+					printf("\nClassificacao Final:  (Masculino Profissional)\n");
+					imprimeClassificacao(grupos[MASCULINO + 2 * PROFISSIONAL], 1);
+					vmp = 2;
+				}
+
+				if(!vfp){
+					printf("\nClassificacao %dmin:  (Feminino Profissional)\n", 30*(lPrint+1));
+					imprimeClassificacao(grupos[FEMININO + 2 * PROFISSIONAL], 0);
+				}
+				else if(vfp==1){
+					printf("\nClassificacao Final:  (Feminino Profissional)\n");
+					imprimeClassificacao(grupos[FEMININO + 2 * PROFISSIONAL], 1);
+					vfp = 2;
+				}
+
+				if(!vma){
+					printf("\nClassificacao %dmin:  (Masculino Amador)\n", 30*(lPrint+1));
+					imprimeClassificacao(grupos[MASCULINO + 2 * PROFISSIONAL], 0);
+				}
+				else if(vma==1){
+					printf("\nClassificacao Final:  (Masculino Amador)\n");
+					imprimeClassificacao(grupos[MASCULINO + 2 * AMADOR], 1);
+					vma = 2;
+				}
+
+				if(!vfa){
+					printf("\nClassificacao %dmin:  (Feminino Amador)\n", 30*(lPrint+1));
+					imprimeClassificacao(grupos[FEMININO + 2 * AMADOR], 0);
+				}
+				else if(vfa==1){
+					printf("\nClassificacao Final:  (Feminino Amador)\n");
+					imprimeClassificacao(grupos[FEMININO + 2 * AMADOR], 1);
+					vfa = 2;
+				}
 			}
-			imprimeClassificacao(lPrint, 0);
 			lPrint++;
 		}
 	}
-
-	while( lPrint<ticMax-1 && !vencedores ){
-		if(debug)
-			printf("\n2Classificacao %dmin:    - %d\n", lPrint+1, ticMax);
-		else
-			printf("\n2Classificacao %dmin:\n", 30*(lPrint));
-		ordenaPosicaoAtleta( tempoEspaco[lPrint], natletas);
-		imprimeClassificacao(lPrint, 0);
-		lPrint++;
-	}
-	printf("\nClassificacao final:\n");
-	ordenaPosicaoAtleta( tempoEspaco[ticMax], natletas);
-	imprimeClassificacao(ticMax, 1);
 
 	return NULL;
 }
