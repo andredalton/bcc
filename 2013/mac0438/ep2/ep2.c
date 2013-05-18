5cec673ce59f54a54cd114349434bb90db5c0b0e
André Meneghelli Vale <avale@kakaroto>
2013-05-18 12:36:11 -0300

    2013 - Concorrente : EP2 finalizando a bellard.

--- 2013/mac0438/ep2/ep2.c
+++ 2013/mac0438/ep2/ep2.c
@@ -29,14 +29,14 @@
 
 short int param;                /* Variável que guarda se roda no modo normal, sequencial ou debug.                     */
 
-short int N;                    /* Diferença entre o termo atual e maior termo no momento de sincronização.             */
-long double buffer;             /* Buffer utilizado para a conversão dos dados fracionários para a estrutura SuperLong. */
-SuperLong N0;                   /* Número do maior termo no momento da sincronização.                                   */
-SuperLong m4;                   /* Variável que acumula parte do cálculo que contém multiplos de 4.                     */
-SuperLong m10;                  /* Variável que acumula parte do cálculo que contém multiplos de 10.                    */
-SuperLong p2;                   /* Variável que acumula parte do cálculo que contém potências de 2.                     */
-SuperLong pi;                   /* Variável para guardar o valor de pi em longa precisão.                               */
-SuperLong precisao;             /* Pecisão do cálculo.                                                                  */
+/*short int N;                    / Diferença entre o termo atual e maior termo no momento de sincronização.             */
+/*long double buffer;             / Buffer utilizado para a conversão dos dados fracionários para a estrutura SuperLong. */
+/*SuperLong N0;                   / Número do maior termo no momento da sincronização.                                   */
+/*SuperLong m4;                   / Variável que acumula parte do cálculo que contém multiplos de 4.                     */
+/*SuperLong m10;                  / Variável que acumula parte do cálculo que contém multiplos de 10.                    */
+/*SuperLong p2;                   / Variável que acumula parte do cálculo que contém potências de 2.                     */
+/*SuperLong pi;                   / Variável para guardar o valor de pi em longa precisão.                               */
+/*SuperLong precisao;             / Pecisão do cálculo.                                                                  */
 
 /*
 sem_t sem_estrada[CITAM];
@@ -44,6 +44,42 @@ sem_t sem_estrada[CITAM];
   /*************************************************/
  /* Termino da declaração das variáveis globais.  */
 /*************************************************/
+
+
+long double pi;                /* Variável para guardar o valor de pi para a máxima precisão da linguagem.             */
+long double precisao;          /* Pecisão do cálculo.                                                                  */
+long int N0;                   /* Número do maior termo no momento da sincronização.                                   */
+long int m4;                   /* Variável que acumula parte do cálculo que contém multiplos de 4.                     */
+long int m10;                  /* Variável que acumula parte do cálculo que contém multiplos de 10.                    */
+long int p2;                   /* Variável que acumula parte do cálculo que contém potências de 2.                     */
+
+long double bellard( int n) {
+	long double termo = 0;
+
+	termo += -32. / (m4+1);				printf("\t[%.100Lf]\n", termo);
+	termo += -1.  / (m4+3);				printf("\t[%.100Lf]\n", termo);
+	termo += 256. / (m10+1);			printf("\t[%.100Lf]\n", termo);
+	termo += -64. / (m10+3);			printf("\t[%.100Lf]\n", termo);
+	termo += -4.  / (m10+5);			printf("\t[%.100Lf]\n", termo);
+	termo += -4.  / (m10+7);			printf("\t[%.100Lf]\n", termo);
+	termo += 1.   / (m10+9);			printf("\t[%.100Lf]\n", termo);
+	termo /= p2;						printf("\t[%.100Lf]\n", termo);
+
+	return (n%2) ? -termo : termo;
+}
+
+long double bellardinho( int n, int k) {
+	switch (k) {
+		case 0: return (n%2) ?   32 /  (m4 + 1 + 4 * (n - N0)) : -32 /  (m4 + 1 + 4 * (n - N0));
+		case 1: return (n%2) ?    1 /  (m4 + 3 + 4 * (n - N0)) :  -1 /  (m4 + 3 + 4 * (n - N0));
+		case 2: return (n%2) ? -256 / (m10 + 3 + 4 * (n - N0)) : 256 / (m10 + 3 + 4 * (n - N0));
+		case 3: return (n%2) ?   64 / (m10 + 3 + 4 * (n - N0)) : -64 / (m10 + 3 + 4 * (n - N0));
+		case 4: return (n%2) ?    4 / (m10 + 3 + 4 * (n - N0)) :  -4 / (m10 + 3 + 4 * (n - N0));
+		case 5: return (n%2) ?    4 / (m10 + 3 + 4 * (n - N0)) :  -4 / (m10 + 3 + 4 * (n - N0));
+		case 6: return (n%2) ?   -1 / (m10 + 3 + 4 * (n - N0)) :   1 / (m10 + 3 + 4 * (n - N0));
+	}
+	return precisao;
+}
 
 /* Função principal.
  *******************/
@@ -54,9 +90,29 @@ int main(int argc, char *argv[]){
 		n,
 		numCPU = sysconf( _SC_NPROCESSORS_ONLN );
 	double d, s=0, d2;
+	long double mem;
 	
-	/* Inicializando variaveis globais. */
+	/* Inicializando variaveis globais.
+	 *********************************/
 	param = 0;
+	pi = 0;
+	N0 = 0;
+	m4 = 0;
+	m10 = 0;
+	p2 = 64;
+
+	for( i=0; i<10; i++){
+		mem = bellard(i);
+		printf("%.100Lf\n", mem);
+		pi += mem;
+
+		m4 += 4;
+		m10 += 10;
+		p2 *= 1024;
+	}
+	printf("\n\n%.100Lf\n", pi);
+
+	return 0;
 
 	if(argc<2) {
 		printf(
@@ -68,6 +124,7 @@ int main(int argc, char *argv[]){
 		);
 	}
 	else {
+		/*
 		for(i=1; i<argc; i++) {
 			if( strcmp(argv[i], "DEBUG")==0 )
 				param = 1;
@@ -81,6 +138,7 @@ int main(int argc, char *argv[]){
 		SuperLongSomaInt(precisao, 1);
 		printSuperLong(precisao);		
 		printf("\n");
+		*/
 	}
 
 	return 0;
