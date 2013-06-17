cd2ae07f945aec414e56f1e603374f2d672fe9f5
Andre Meneghelli Vale <avale@kakaroto>
2013-06-17 00:26:22 -0300

    2013 - Concorrente : EP3 Melhoria no título dos gráficos.

--- 2013/mac0438/ep3/script.ep3.sh
+++ 2013/mac0438/ep3/script.ep3.sh
@@ -2,12 +2,18 @@
 
 mkdir -p ./graficos
 
-for H in {1..10}; do
-   for B in {1..10}; do
-      for N in {1..10}; do
-         for T in {1..2..100}; do
-            for t in {1..10}; do
-               python ./ep3.py $N $B ${H} $t $T -g >/dev/null;
+HV='1 50 100 150 200'
+BV='1 10 30'
+NV='1 50 100 150 200'
+TV='1 40 200'
+tV='1 10 100'
+
+for H in $HV; do
+   for B in $BV; do
+      for N in $NV; do
+         for T in $TV; do
+            for t in $tV; do
+               python ./ep3.py $N $B $H $t $T -g >/dev/null;
                echo -e "\nOK $N $B $H $t $T\n";
             done
          done
