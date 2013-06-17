5d67d83a480cb93e3ee6479a8bd070738359e95e
Andre Meneghelli Vale <avale@kakaroto>
2013-06-17 01:32:23 -0300

    2013 - Concorrente : EP3 Relatório quase concluído.

--- 2013/mac0438/ep3/script.ep3.sh
+++ 2013/mac0438/ep3/script.ep3.sh
@@ -2,17 +2,11 @@
 
 mkdir -p ./graficos
 
-HV='1 50 100 150 200'
-BV='1 10 30'
-NV='1 50 100 150 200'
-TV='1 40 200'
-tV='1 10 100'
-
-for H in $HV; do
-   for B in $BV; do
-      for N in $NV; do
-         for T in $TV; do
-            for t in $tV; do
+for H in {1..200..50}; do
+   for B in [1 10 30]; do
+      for N in {1..200..50}; do
+         for T in [1 40 200]; do
+            for t in [1 10 100]; do
                python ./ep3.py $N $B $H $t $T -g >/dev/null;
                echo -e "\nOK $N $B $H $t $T\n";
             done
