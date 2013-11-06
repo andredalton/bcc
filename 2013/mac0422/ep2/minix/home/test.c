809ef1a1d0a8df5f72c321470af9095f356b0f5d
Andre Meneghelli Vale <avale@sanji>
2013-11-06 15:52:33 -0200

    2013 - SO : EP2 Calls respondendo e recebendo os parametros.

--- 2013/mac0422/ep2/minix/home/test.c
+++ 2013/mac0422/ep2/minix/home/test.c
@@ -1,7 +1,11 @@
-#include<mycalllib.h>
+#include<semaforo.h>
 
 int main (void)
 {
-	mycall();
+	get_sem(5);
+	p_sem(4);
+	v_sem(3);
+	free_sem(2);
+
 	return 0;
 }
--- 2013/mac0422/ep2/minix/usr/include/mycalllib.h
+++
@@ -1,7 +0,0 @@
-#include <lib.h>
-#include <unistd.h>
-
-PUBLIC int mycall () {
-	message m;
-	return (_syscall(PM_PROC_NR, MYCALL, &m) );
-}
