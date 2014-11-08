6ba79549f7b4eebc3fe35f7f6712791fdf71e6cf
André Meneghelli Vale <andredalton@gmail.com>
2014-11-08 17:06:50 -0200

    2014 - LABPROG2 - Miniep5: BUG ao utilizar variáveis.

--- 2014/MAC0242/miniep5/miniep5.py
+++ 2014/MAC0242/miniep5/miniep5.py
@@ -3,6 +3,9 @@
 # Apenas para poder manipular a linha de comando
 import sys
 
+# Para poder verificar se é uma expressão vazia.
+import re
+
 # Manipulacao do historico de comandos para facilitar os testes.
 import readline
 histfile = ".history"
@@ -19,30 +22,49 @@ import ply.lex as lex
 import ply.yacc as yacc
     
 class Calc:
-    def __init__(self):
+    def __init__(self, transmite_erros=False):
+        self.transmite_erros = transmite_erros
+        self.ctokens = 0
+        self.resposta = None
         self.names = { }
         lex.lex(module=self)
         yacc.yacc(module=self)
 
-    def analisar(self, str):
-        return yacc.parse(str)
-        
+    def analisar(self, s):
+        self.resposta = None
+        # Só realiza a busca se exitir algum caracter não branco.
+        if re.search("[^\s]", s) is not None:
+            yacc.parse(s)
+        return self.resposta
 
     # Definicao dos tokens
-    tokens = ( 'NOME','NUMERO', 'SOM','SUB', 'MUL','DIV','ATRIB' )
+    tokens = (
+        'NOME','NUMERO',
+        'SOM','SUB', 'MUL','DIV','ATRIB',
+        )
 
     t_SOM     = r'\+'
     t_SUB     = r'-'
     t_MUL     = r'\*'
     t_DIV     = r'/'
     t_ATRIB   = r'='
-    t_NOME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    
+    def t_NOME(self, t):
+        r'[a-zA-Z_][a-zA-Z0-9_]*'
+        print(t.value)
+        try:
+            self.names[t.value]
+        except KeyError:
+            self.names[t.value] = None
+        return t
 
     def t_NUMERO(self, t):
         r'\d+\.?\d*[eE][+\-]?\d+|\d+\.?\d*'
+        print(t.value)
         try:
             t.value = float(t.value)
         except ValueError:
+            if self.transmite_erros: raise
             print("Por favor, digite apenas numeros validos:", t.value[0])
             t.value = 0
         return t
@@ -51,63 +73,68 @@ class Calc:
     t_ignore = " \t"
 
     def t_error(self, t):
+        print("t_error", t.value)
+        if self.transmite_erros: raise TypeError
         print("Caracter incorreto '%s'" % t.value)
         t.lexer.skip(1)
 
     # Regras de analise sintatica
     precedence = (
-        ('left','SOM','SUB'),
-        ('left','MUL','DIV')
+        ('right', 'NOME', 'NUMERO'),
         )
-
-    def p_atribuicao(self, p):
-        'statement : NOME ATRIB expression'
-        self.names[p[1]] = p[3]
 
     def p_expressao(self, p):
         'statement : expression'
-        print(p[1])
+        print("p_expressao", p[1])
+        self.resposta = p[1]
 
     def p_expressao_dupla(self, p):
         """
         expression : expression expression SOM
-                  | expression expression SUB
-                  | expression expression MUL
-                  | expression expression DIV
+                   | expression expression SUB
+                   | expression expression MUL
+                   | expression expression DIV
+                   | NOME expression ATRIB
         """
-        if p[1] is None or p[2] is None:
-            p[0] = None
-        elif p[3] == '+'  : p[0] = p[1] + p[2]
-        elif p[3] == '-': p[0] = p[1] - p[2]
-        elif p[3] == '*': p[0] = p[1] * p[2]
-        elif p[3] == '/':
-            try:
-                p[0] = p[1] / p[2]
-            except ZeroDivisionError:
-                print("Divisao por zero!")
-
-
-    def p_numero(self, p):
-        'expression : NUMERO'
-        p[0] = p[1]
+        print("p_expressao_dupla", p[1], p[2], p[3])
+        if p[1] is not None and p[2] is not None:
+            if p[3]   == '=': p[0] = self.names[p[1]] = p[2]
+            elif p[3] == '+': p[0] = p[1] + p[2]
+            elif p[3] == '-': p[0] = p[1] - p[2]
+            elif p[3] == '*': p[0] = p[1] * p[2]
+            elif p[3] == '/':
+                try:
+                    p[0] = p[1] / p[2]
+                except ZeroDivisionError:
+                    if self.transmite_erros: raise
+                    print("Divisao por zero!")
 
     def p_nome(self, p):
         'expression : NOME'
-        try:
-            p[0] = self.names[p[1]]
-        except LookupError as e:
+        print("p_nome", p[1])
+        p[0] = self.names[p[1]]
+        if self.names[p[1]] is None:
+            if self.transmite_erros: raise KeyError
             print("Variavel '%s' sem atribuicao" % p[1])
 
+    def p_numero(self, p):
+        'expression : NUMERO'
+        print("p_numero", p[1])
+        p[0] = p[1]
+
     def p_error(self, p):
+        print("p_erro")
+        if self.transmite_erros: raise LookupError
         print("Erro de sintaxe!")
 
 if __name__ == '__main__':
     s = ' '.join(sys.argv[1:])
     f = len(s) != 0
-    calc = Calc()
+    calc = Calc(f)
     while 1:
-        if len(s) > 0:
-            calc.analisar(s)
+        r = calc.analisar(s)
+        if r is not None:
+            print("%f" % r)
         if f: break
         try:
             s = input('calc > ')
--- 2014/MAC0242/miniep5/test.py
+++ 2014/MAC0242/miniep5/test.py
@@ -2,8 +2,7 @@
 
 import os, random, unittest, subprocess
 
-def run(s):
-    return  subprocess.getoutput("./miniep5.py " + s ).replace("Generating LALR tables\n", "")
+from miniep5 import Calc
 
 def gera_var():
     p = ""
@@ -15,114 +14,114 @@ def gera_teste(num, op):
     lst = []
     i = 0
     j = 0
-    while i < num and j < op:
+    p = random.random() # Probabilidade de um operador ser inserido no início da expressao
+    tokens = ["+", "-", "*", "/"]
+    while i < num:
         r = random.random()
-        if i > j + 1 and r < 0.6:
-            lst.append( random.choice(["+", "-", "*", "/"]) )
+        if i > j + 1 and r < p:
+            lst.append( random.choice(tokens) )
             j += 1
         lst.append( str(random.random()*10**random.randint(-30,30)) )
         i += 1
     for j in range(j, op):
-        lst.append( random.choice(["+", "-", "*", "/"]) )
-    return " ".join(["\""] + lst + ["\""] )
+        lst.append( random.choice(tokens) )
+    return " ".join(lst)
     
 class Test(unittest.TestCase):
+    def setUp(self):
+        """ Inicializa a calculadora."""
+        self.calc = Calc(True)
+
     # Testando inf, -inf e nan
     def test_inf(self):
-        """ Verifica a comutatividade da soma."""
-        out = run("1" * 1000)
-        self.assertEqual( out, "inf" )
+        """ Verifica a ocorrencia do inf."""
+        inf = self.calc.analisar("1" * 1000)
+        self.assertEqual( inf, float("inf") )
     
     def test_minf(self):
-        """ Verifica a comutatividade da soma."""
-        out = run(' '.join(["0", "1" * 1000, "-"]))
-        self.assertEqual( out, "-inf" )
+        """ Verifica a ocorrencia do -inf."""
+        minf = self.calc.analisar("0 %s -" % ("1"*1000))
+        self.assertEqual( minf, float("-inf") )
 
     def test_nan(self):
-        """ Verifica a comutatividade da soma."""
-        out = run(' '.join(["1" * 1000, "0", "1" * 1000, "- +"]))
-        self.assertEqual( out, "nan" )
+        """ Verifica a ocorrencia do nan."""
+        out = self.calc.analisar("%(b)s 0 %(b)s - +" % {'b': "1"*1000})
+        self.assertNotEqual( out, out )
 
     # Testando propriedades básicas das operações.
     def test_som_comutativa(self):
         """ Verifica a comutatividade da soma."""
-        a = random.random()
-        b = random.random()
-        aout = run(' '.join(["\"", str(a), str(b), "+\""]))
-        bout = run(' '.join(["\"", str(b), str(a), "+\""]))
+        dic = {'a': random.random(), 'b': random.random()}
+        aout = self.calc.analisar("%(a)f %(b)f +" % dic)
+        bout = self.calc.analisar("%(b)f %(a)f +" % dic)
         self.assertEqual( aout, bout )
 
     def test_som_elemento_neutro(self):
         """ Verifica o elemento neutro da soma."""
         a = random.random()
-        aout = run(' '.join(["\"", str(a), "0 +\""]))
-        self.assertEqual( a, float(aout) )
+        aout = self.calc.analisar("%.100f 0 +" % a)
+        self.assertEqual( a, aout )
 
     def test_som_anulamento(self):
         """ Verifica o anulamento da soma."""
-        a = random.random()
-        aout = run(' '.join(["\"", str(a), "0", str(a)," - +\""]))
-        self.assertEqual( 0, float(aout) )
+        dic = {'a': random.random()}
+        aout = self.calc.analisar("%(a)f 0 %(a)f - +" %dic)
+        self.assertEqual( 0, aout )
 
     def test_sub_comutativa(self):
         """ Verifica a não comutatividade da subtração."""
-        a = random.random()
-        b = random.random()
-        aout = run(' '.join(["\"", str(a), str(b), "-\""]))
-        bout = run(' '.join(["\"", str(b), str(a), "-\""]))
+        dic = {'a': random.random(), 'b': random.random()}
+        aout = self.calc.analisar("%(a)f %(b)f -" % dic)
+        bout = self.calc.analisar("%(b)f %(a)f -" % dic)
         self.assertNotEqual( aout, bout )
 
     def test_sub_elemento_neutro(self):
-        """ Verifica o elemento neutro da soma."""
+        """ Verifica o elemento neutro da subtração."""
         a = random.random()
-        aout = run(' '.join(["\"", str(a), "0 -\""]))
-        self.assertEqual( a, float(aout) )
+        aout = self.calc.analisar("%.100f 0 -" % a)
+        self.assertEqual( a, aout )
 
-    def test_som_anulamento(self):
-        """ Verifica o anulamento da soma."""
-        a = random.random()
-        aout = run(' '.join(["\"", str(a), str(a)," -\""]))
-        self.assertEqual( 0, float(aout) )
+    def test_sub_anulamento(self):
+        """ Verifica o anulamento da subtração."""
+        dic = {'a': random.random()}
+        aout = self.calc.analisar("%(a)f %(a)f -" % dic)
+        self.assertEqual( 0, aout )
 
     def test_mul_comutativa(self):
         """ Verifica a comutatividade da multiplicacao."""
-        a = random.random()
-        b = random.random()
-        aout = run(' '.join(["\"", str(a), str(b), "*\""]))
-        bout = run(' '.join(["\"", str(b), str(a), "*\""]))
+        dic = {'a': random.random(), 'b': random.random()}
+        aout = self.calc.analisar("%(a)f %(b)f *" % dic)
+        bout = self.calc.analisar("%(a)f %(b)f *" % dic)
         self.assertEqual( aout, bout )
 
     def test_mul_elemento_neutro(self):
         """ Verifica o elemento neutro da multiplicacao."""
         a = random.random()
-        aout = run(' '.join(["\"", str(a), "1 *\""]))
-        self.assertEqual( a, float(aout) )
+        aout = self.calc.analisar("%.100f 1 *" % a)
+        self.assertEqual( a, aout )
 
     def test_mul_elemento_nulo(self):
         """ Verifica o elemento nulo da multiplicacao."""
-        a = random.random()
-        aout = run(' '.join(["\"", str(a), "0 *\""]))
-        self.assertEqual( 0, float(aout) )
+        aout = self.calc.analisar("%.100f 0 *" % random.random())
+        self.assertEqual( 0, aout )
 
     def test_div_comutativa(self):
         """ Verifica a não comutatividade da divisão."""
-        a = random.random()
-        b = random.random()
-        aout = run(' '.join(["\"", str(a), str(b), "/\""]))
-        bout = run(' '.join(["\"", str(b), str(a), "/\""]))
+        dic = {'a': random.random(), 'b': random.random()}
+        aout = self.calc.analisar("%(a)f %(b)f /" % dic)
+        bout = self.calc.analisar("%(b)f %(a)f /" % dic)
         self.assertNotEqual( aout, bout )
 
     def test_div_elemento_neutro(self):
         """ Verifica o elemento neutro da divisão."""
-        a = random.random()
-        aout = run(' '.join(["\"", str(a), "1 /\""]))
-        self.assertEqual( a, float(aout) )
+        a = random.random()        
+        aout = self.calc.analisar("%.100f 1 /" % a)
+        self.assertEqual( a, aout )
 
     def test_div_zero(self):
         """ Verifica a divisao por zero."""
         a = random.random()
-        aout = run(' '.join(["\"", str(a), "0 /\""]))
-        self.assertEqual( aout, "Divisao por zero!\nNone" )
+        self.assertRaises(ZeroDivisionError, self.calc.analisar, "%.100f 0 /" % a)
 
     # Testes de sintaxe.
     def test_sintaxe(self):
@@ -132,8 +131,8 @@ class Test(unittest.TestCase):
         """
         n = random.randint(1, 1000)
         s = gera_teste(n, n-1)
-        try:
-            out = float(run(s))
+        try:            
+            out = float(self.calc.analisar(s))
             conv = True
         except ValueError:
             conv = False
@@ -142,28 +141,22 @@ class Test(unittest.TestCase):
     def test_erro_sintaxe1(self):
         """ Verifica erros de sintaxe quando existem mais numeros do que operações - 1."""
         n = random.randint(1, 1000)
-        s = gera_teste(n + random.randint(1, 100), n-1)
-        out = run(s)
-        self.assertRaises(ValueError, float, out)
+        s = gera_teste(n + random.randint(2, 100), n)
+        self.assertRaises(LookupError, self.calc.analisar, s)
 
     def test_erro_sintaxe1(self):
         """ Verifica erros de sintaxe quando existem menos numeros do que operações - 1."""
         n = random.randint(1, 1000)
         s = gera_teste(n, n + random.randint(0, 100))
-        out = run(s)
-        self.assertRaises(ValueError, float, out)
+        self.assertRaises(LookupError, self.calc.analisar, s)
 
     def test_caracter_incorreto(self):
         """ Verifica a ocorrencia de erro quando se utiliza um caracter não especificado."""
-        c = random.choice(["!", "@", "$", "?"])
-        out = run( c )
-        self.assertEqual( out, "Caracter incorreto '%c'\nErro de sintaxe!" % c)
+        self.assertRaises(TypeError, self.calc.analisar, random.choice(["!", "@", "$", "?"]) )
 
     def test_variavel_nao_inicializada(self):
-        """ Verifica a ocorrencia de erro quando se utiliza um caracter não especificado."""
-        s = gera_var()
-        out = run( s )
-        self.assertEqual( out, "Variavel '%s' sem atribuicao\nNone" % s)
+        """ Verifica a ocorrencia de erro quando se utiliza uma variável não inicializa."""
+        self.assertRaises(KeyError, self.calc.analisar, gera_var())
 
 if __name__ == '__main__':
     unittest.main()
